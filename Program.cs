2d7e755d97ee0859d862fb70673d38d6e5f57ae5
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-22 11:24:23 -0300

    text edited

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace FirstProjectDotNet
             int times = 5;
 
             for(int i = 1; i <= times; i ++){
-                Console.WriteLine($"Esse é meu primeiro projeto {i}");
+                Console.WriteLine($"Lista de Números {i}");
             }
         }
     }
